bcff395064d85b30f39bb867794a204fd2b9aaba
Kitz24 <krishnaroop0710@gmail.com>
2024-03-20 19:11:02 +0530

    finished items

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,21 +4,62 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <div className="hero border-1 pb-3">
-        <div className="card bg-dark text-white border-0 mx-3">
+      <div
+        className="hero border-1 pb-3"
+        style={{
+          position: "relative",
+          width: "100%",
+          height: "500px",
+        }}
+      >
+        <div
+          className="card bg-dark text-white border-0 mx-3"
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        >
           <img
             className="card-img img-fluid.max-width:100%"
             src="./assets/main.png.jpg"
             alt="Card"
             height={500}
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              filter: "brightness(100%)",
+            }}
           />
-          <div className="card-img-overlay d-flex align-items-center">
+          <div
+            className="card-img-overlay d-flex align-items-center"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              padding: "3rem",
+              boxSizing: "border-box",
+              backgroundColor: "rgba(0, 0, 0, 0.5)",
+            }}
+          >
             <div className="container text-container">
-              <h1 className="card-title fs-1 text fw-bold">SPECIAL OFFERS INCOMING</h1>
-              <p className="card-text fs-1 fw-bold d-none d-sm-block">
-                NEW OFFERS FOR SPECIAL OCCATIONS AND CELEBRATIONS.
+              <h1 className="card-title fs-1 text fw-bold" style={{ color: "white" }}>
+                SPECIAL OFFERS INCOMING
+              </h1>
+              <p className="card-text fs-1  d-none d-sm-block" style={{ color: "yellow" }}>
+                <em>New offers for special occations and celebrations.</em>
               </p>
-              <p className="card-text fs-1 fw-bold d-none d-sm-block" >UNLEASH YOUR INNER CHEF WITH AMARIS CUTLERY.</p>
+              <p className="card-text fs-1 d-none d-sm-block" style={{ color: "white" }}>
+                <em>Unleash your inner chef with Amaris Cutlery.</em>
+              </p>
             </div>
           </div>
         </div>
