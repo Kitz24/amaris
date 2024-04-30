93c1f77e219c490075d3309fe2a8ebc1c77776c9
Kitz24 <krishnaroop0710@gmail.com>
2024-04-30 20:07:32 +0530

    almost done login

--- src/pages/ForgotPass.jsx
+++ src/pages/ForgotPass.jsx
@@ -13,14 +13,16 @@ function ForgotPass() {
     const navigate = useNavigate();  
 
     useEffect(() => {
-        const authListener = supabase.auth.onAuthStateChange((event, session) => {
-          setSession(session);
-          if(session != null){
-            // Store session data in localStorage
-            localStorage.setItem('session', JSON.stringify(session));
-            navigate('/profile');
-          }
-        });
+      supabase.auth.onAuthStateChange(async (event, session) => {
+        if (event == "PASSWORD_RECOVERY") {
+          const newPassword = prompt("What would you like your new password to be?");
+          const { data, error } = await supabase.auth
+            .updateUser({ password: newPassword })
+   
+          if (data) alert("Password updated successfully!")
+          if (error) alert("There was an error updating your password.")
+        }
+      })
       }, []);
 
 
